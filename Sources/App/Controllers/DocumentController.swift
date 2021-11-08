16b176212d2c87995379ed634e6b3fc931b16d15
Connor Black <connor.b645@gmail.com>
2021-11-08 22:25:04 +0000

    Removed unused stream upload function

--- Sources/App/Controllers/DocumentController.swift
+++ Sources/App/Controllers/DocumentController.swift
@@ -10,7 +10,6 @@ import Vapor
 
 struct DocumentController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
-        
         let documentRoute = routes.grouped("documents")
         
         let bearerAuthProtected = documentRoute.grouped(Token.authenticator())
@@ -32,54 +31,6 @@ struct DocumentController: RouteCollection {
                 
         return try await req.fs.upload(key: path, data: data).get()
     }
-    
-//    func upload(req: Request) throws -> EventLoopFuture<Document.Public> {
-//
-//        try req.auth.require(User.self)
-//
-//        let fileName = try filename(with: req.headers)
-//        let document = Document(documentName: fileName)
-//        let path = document.path(for: req.application)
-//
-//        let statusPromise = req.eventLoop.makePromise(of: Document.Public.self)
-//
-//        req.body.drain { someResult -> EventLoopFuture<Void> in
-//            let drainPromise = req.eventLoop.makePromise(of: Void.self)
-//
-//            switch someResult {
-//            case .buffer(let buffer):
-//                 _ = req.fileio.writeFile(buffer, at: path)
-//                    .always { outcome in
-//                        switch outcome {
-//                        case .success(let yep):
-//                            return drainPromise.succeed(yep)
-//                        case .failure(let nope):
-//                            return drainPromise.fail(nope)
-//                        }
-//                    }
-//            case .error(let e):
-//                try? FileManager.default.removeItem(atPath: path)
-//                statusPromise.fail(e)
-//                drainPromise.fail(e)
-//            case .end:
-//                _ = document.save(on: req.db)
-//                    .flatMapThrowing { _ in
-//                        try document.asPublic()
-//                    }.always { result in
-//                        switch result {
-//                        case .success(let publicDocument):
-//                            statusPromise.succeed(publicDocument)
-//                        case .failure(let error):
-//                            statusPromise.fail(error)
-//                        }
-//                    }
-//                drainPromise.succeed(())
-//            }
-//
-//            return drainPromise.futureResult
-//        }
-//        return statusPromise.futureResult
-//    }
     
     func filename(with headers: HTTPHeaders) throws -> String {
         let fileExt = try fileExtension(for: headers)
